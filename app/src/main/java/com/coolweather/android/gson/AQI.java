a4095371ae61452585e0c207c4262da6e41b2849
JnoSnow6 <991665518@qq.com>
2018-02-05 14:19:55 +0800

    加入显示天气信息的功能.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by LZC on 2018/2/1.
+ */
+
+public class AQI {
+
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
